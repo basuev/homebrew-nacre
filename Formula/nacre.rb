e9764b25c30398688039822801d32fdd6f6bbb83
timofey basuev <t.basuev@bronevik.com>
2026-03-22 13:18:49 +0500

    nacre 0.6.0

--- Formula/nacre.rb
+++ Formula/nacre.rb
@@ -1,8 +1,8 @@
 class Nacre < Formula
   desc "minimal POSIX shell with fish-like autosuggestions"
   homepage "https://github.com/basuev/nacre"
-  url "https://github.com/basuev/nacre/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "00b1d76aa78b9593f81fbabdc7ef7e2c424c4cb3a2edae5d3bc46cea6ab6e763"
+  url "https://github.com/basuev/nacre/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "14587a8d0583c6233af0aeb7c8908ad8f366bb90999eb6b63d7ba746083ccdbf"
   license "MIT"
 
   def install
